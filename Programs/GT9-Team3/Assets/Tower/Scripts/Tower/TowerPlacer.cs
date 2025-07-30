f115c76a36b65e43bf214b81e0a2e93e0a229549
김원진 <89440747+dnjswwls@users.noreply.github.com>
2025-07-31 03:41:56 +0900

    feat: 타일 그리드에 맞게 타워 설치 기능 구현

--- Programs/GT9-Team3/Assets/Tower/Scripts/Tower/TowerPlacer.cs
+++ Programs/GT9-Team3/Assets/Tower/Scripts/Tower/TowerPlacer.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Resources;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class TowerPlacer : MonoBehaviour
 {
@@ -10,38 +10,58 @@ public class TowerPlacer : MonoBehaviour
     public GameObject towerPrefab;
     public TowerData towerData;
 
-    public Grid grid;   // 타워 타일 위에 설치하기
+    [SerializeField] private Tilemap groundTilemap; // Ground 타일맵 참조
+    public Grid grid; // 타워 타일 위에 설치하기
+
+    public GameObject previewPrefab;
+    private GameObject currentPreview;
+
+    private HashSet<Vector3Int> occupiedCells = new HashSet<Vector3Int>();
+
+    void Start()
+    {
+        currentPreview = Instantiate(previewPrefab);
+    }
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        Vector3 worldPos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        worldPos.z = 0;
+
+        Vector3Int cellPos = grid.WorldToCell(worldPos);
+        Vector3 snappedPos = grid.GetCellCenterWorld(cellPos);
+        snappedPos.z = 0;
+
+        // 미리보기 위치 갱신
+        Vector3 previewPos = snappedPos;
+        previewPos.y += 0.44f;
+        currentPreview.transform.position = previewPos;
+
+        // 설치 가능 여부에 따라 색상 변경
+        bool canPlace = CanPlaceTowerAt(cellPos);
+        SpriteRenderer sr = currentPreview.GetComponent<SpriteRenderer>();
+        sr.color = canPlace ? new Color(1, 1, 1, 0.5f) : new Color(1, 0, 0, 0.5f);
+
+        // 클릭해서 설치
+        if (Input.GetMouseButtonDown(0) && canPlace) // && ResourceManager.Instance.CanAfford(...)
         {
-            Vector3 worldPos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
-            worldPos.z = 0;
-
-            Vector3Int cellPos = grid.WorldToCell(worldPos);          // 클릭한 셀 좌표
-            Vector3 snappedPos = grid.CellToWorld(cellPos);           // 셀 기준 월드 위치
-            snappedPos.z = 0;                                         // Z축 고정
-
-            if (CanPlaceTowerAt(cellPos) && ResourceManager.Instance.CanAfford(towerData.makeCost, towerData.makeValue))
-            {
-                PlaceTower(snappedPos, cellPos);
-            }
+            PlaceTower(snappedPos, cellPos);
         }
     }
 
-    bool CanPlaceTowerAt(Vector3 position)
+    bool CanPlaceTowerAt(Vector3Int cellPos)
     {
-        // 타일맵, collider, 기타 설치 가능 조건 검사
-        // 지금은 무조건 가능하게
-        return true;
+        TileBase tile = groundTilemap.GetTile(cellPos);
+        return tile != null && !occupiedCells.Contains(cellPos);
     }
 
-    void PlaceTower(Vector3 position)
+    void PlaceTower(Vector3 position, Vector3Int cellPos)
     {
-        Debug.Log($"[타워 설치됨] 위치: {position}");
+        // 타워를 타일보다 약간 위에 배치
+        position.y += 0.44f;
 
         GameObject tower = Instantiate(towerPrefab, position, Quaternion.identity);
+        tower.GetComponent<SpriteRenderer>().sortingOrder = -(int)(position.y * 100);
 
         Tower1 towerScript = tower.GetComponent<Tower1>();
         if (towerScript != null)
@@ -52,22 +72,6 @@ public class TowerPlacer : MonoBehaviour
         {
             Debug.LogError("[오류] Tower1 스크립트가 프리팹에 없음");
         }
-
-        ResourceManager.Instance.Spend(towerData.makeCost, towerData.makeValue);
-    }
-
-    private HashSet<Vector3Int> occupiedCells = new HashSet<Vector3Int>();
-
-    bool CanPlaceTowerAt(Vector3Int cellPos)
-    {
-        return !occupiedCells.Contains(cellPos);
-    }
-
-    void PlaceTower(Vector3 position, Vector3Int cellPos)
-    {
-        GameObject tower = Instantiate(towerPrefab, position, Quaternion.identity);
-        Tower1 towerScript = tower.GetComponent<Tower1>();
-        towerScript.ApplyData(towerData);
 
         ResourceManager.Instance.Spend(towerData.makeCost, towerData.makeValue);
         occupiedCells.Add(cellPos);
