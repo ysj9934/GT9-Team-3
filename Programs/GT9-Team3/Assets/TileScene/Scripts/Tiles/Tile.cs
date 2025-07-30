6086d105cf96f8af1d0c3877bebb7378fee4d5f1
ysj9934 <85571369+ysj9934@users.noreply.github.com>
2025-07-30 16:45:55 +0900

    cell grid move

--- Programs/GT9-Team3/Assets/TileScene/Scripts/Tiles/Tile.cs
+++ Programs/GT9-Team3/Assets/TileScene/Scripts/Tiles/Tile.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float cellX = 1f;
+    [SerializeField] private float cellY = 1f;
+    
+    void OnMouseDrag()
     {
-        
+        Plane plane = new Plane(Vector3.forward, 0);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (plane.Raycast(ray, out float distance))
+        {
+            Vector3 point = ray.GetPoint(distance);
+            transform.position = SnapToGrid(point); // 격자 스냅 함수
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    Vector3 SnapToGrid(Vector3 pos)
     {
-        
+        float x = Mathf.Round(pos.x / cellX) * cellX;
+        float y = Mathf.Round(pos.y / cellY) * cellY;
+        return new Vector3(x, y, pos.z);
     }
+
 }
