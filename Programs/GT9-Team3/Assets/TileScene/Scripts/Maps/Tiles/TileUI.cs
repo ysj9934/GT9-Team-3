c4a7aa33f4d291dbabdcd33d00016814becbefc8
ysj9934 <85571369+ysj9934@users.noreply.github.com>
2025-08-21 20:31:51 +0900

    fix: tile UI

--- Programs/GT9-Team3/Assets/TileScene/Scripts/Maps/Tiles/TileUI.cs
+++ Programs/GT9-Team3/Assets/TileScene/Scripts/Maps/Tiles/TileUI.cs
@@ -30,14 +30,56 @@ public class TileUI : MonoBehaviour
         _tileInfo = GetComponent<TileInfo>();
     }
 
-    private void OnMouseDown()
+    // private void OnMouseDown()
+    // {
+    //     if (EventSystem.current.IsPointerOverGameObject())
+    //         return;
+    //     
+    //     _tileInfo._tileManager.CloseAllUI(this);
+    //     
+    //     tileUI.SetActive(!tileUI.activeSelf);
+    // }
+    
+    private void Update()
     {
-        if (EventSystem.current.IsPointerOverGameObject())
-            return;
-        
-        _tileInfo._tileManager.CloseAllUI(this);
-        
-        tileUI.SetActive(!tileUI.activeSelf);
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (EventSystem.current.IsPointerOverGameObject())
+                return;
+            
+            int layerMask = LayerMask.GetMask("Ground");
+            
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero, Mathf.Infinity, layerMask);
+            
+            // Debug.DrawRay(mousePos, Vector2.zero, Color.red, 1f);
+            //
+            // Debug.Log("Mouse Pos: " + mousePos);
+            // if (hit.collider != null)
+            // {
+            //     Debug.Log("Hit: " + hit.collider.name);
+            // }
+            // else
+            // {
+            //     Debug.Log("No hit detected");
+            // }
+
+            if (hit.collider != null)
+            {
+                Debug.Log("Hit: " + hit.collider.name);
+                
+                TileInfo tileInfo = hit.collider.GetComponent<TileInfo>();
+                if (tileInfo != null)
+                {
+                    _tileInfo._tileManager.CloseAllUI(this);
+                    this.tileUI.SetActive(!tileUI.activeSelf);
+                }
+            }
+            else
+            {
+                _tileInfo._tileManager.CloseAllUI(null);
+            }
+        }
     }
 
     public void CloseUI()
