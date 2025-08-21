e3db3fb8f12b9e74dd9e4060a29b17e31fd0912a
김원진 <89440747+dnjswwls@users.noreply.github.com>
2025-08-22 03:10:21 +0900

    feat: 타워 종류 5가지 UI 구현
    타워 선택 UI 구현(임시)

--- Programs/GT9-Team3/Assets/Tower/Scripts/Tower/TowerSellUI.cs
+++ Programs/GT9-Team3/Assets/Tower/Scripts/Tower/TowerSellUI.cs
@@ -11,6 +11,12 @@ public class TowerSellUI : MonoBehaviour
 
     void Awake()
     {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
         Instance = this;
         panel.SetActive(false);
     }
