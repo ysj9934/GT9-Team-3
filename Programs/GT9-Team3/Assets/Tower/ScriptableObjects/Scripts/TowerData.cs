2f1f04b35cebe373ba6717b013b770b512ecd291
ysj9934 <85571369+ysj9934@users.noreply.github.com>
2025-08-22 19:53:51 +0900

    feat: file

--- Programs/GT9-Team3/Assets/Tower/ScriptableObjects/Scripts/TowerData.cs
+++ Programs/GT9-Team3/Assets/Tower/ScriptableObjects/Scripts/TowerData.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditorInternal.Profiling.Memory.Experimental.FileFormat;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "NewTowerData", menuName = "Tower Defense/Tower Data")]
